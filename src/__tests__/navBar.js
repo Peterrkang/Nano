8ee2fcb3a3a7adc9c8a33d091d045414a5344d89
Peter <Peterrkang@gmail.com>
2018-07-26 17:15:09 -0700

    update navbar tests

--- src/__tests__/navBar.js
+++ src/__tests__/navBar.js
@@ -1,18 +1,31 @@
-import React from "react"
-import NavBar from "../navBar.js"
-import { shallow } from 'enzyme'
+import React from "react";
+import NavBar from "../navBar.js";
+import { shallow, mount } from "enzyme";
 
-describe('<NavBar />', () => {
+describe("<NavBar />", () => {
+  it("render navbar", () => {
+    const navBar = shallow(<NavBar />);
+    expect(navBar).toHaveLength(1);
+  });
 
-  it('input is empty', () => {
-    const wrapper = shallow(<NavBar />)
-    const inputValueState = wrapper.state().inputValue
-    expect(inputValueState).toEqual("")
-  })
+  it("stimulates view button click", () => {
+    const handleSwitch = jest.fn();
+    const wrapper = mount(<NavBar handleViewSwitch={handleSwitch} />);
+    expect(handleSwitch).toHaveBeenCalledTimes(0);
+    const viewButton = wrapper.find("i").simulate("click");
+    expect(handleSwitch).toHaveBeenCalledTimes(1);
+  });
 
-  it('input will change', () => {
-    const wrapper = shallow(<NavBar />)
-    const input = wrapper.find('input')
-    input.simulate('change')
-  })
-})
+  it("stimulates input change", () => {
+    const handleSearch = jest.fn();
+    const event = {
+      target: {
+        value: "abc"
+      }
+    };
+    const wrapper = mount(<NavBar handleInputSearch={handleSearch} />);
+    expect(wrapper.state().inputValue).toBe("");
+    const inputValue = wrapper.find("input").simulate("change", event);
+    expect(wrapper.state().inputValue).toBe("abc");
+  });
+});
